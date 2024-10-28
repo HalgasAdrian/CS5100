b8a5b1cd513dd6aa9216b245b55e7c39577844ea
Adrian Halgas <halgas.a@northeastern.edu>
2024-10-28 14:59:26 -0400

    Update MFMC.py
    Autograder changes

--- Programming Assignment 2/code/MFMC.py
+++ Programming Assignment 2/code/MFMC.py
@@ -125,13 +125,13 @@ def Q_learning(num_episodes=10000, gamma=0.9, epsilon=1, decay_rate=0.999):
                 refresh(obs, reward, done, info)
 
         epsilon *= decay_rate
-        print("Completed episode", episode)
+        #print("Completed episode", episode)
 
     return Q_table
 
-decay_rate = 0.99
+decay_rate = 0.999999
 
-Q_table = Q_learning(num_episodes=30000, gamma=0.9, epsilon=1, decay_rate=decay_rate) # Run Q-learning
+Q_table = Q_learning(num_episodes=1000000, gamma=0.9, epsilon=1, decay_rate=decay_rate) # Run Q-learning
 
 # Save the Q-table dict to a file
 with open('Q_table.pickle', 'wb') as handle:
